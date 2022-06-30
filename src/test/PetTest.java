cf852864aba183c7c8c5a0277a0bd937d36fe33d
Dmitry-757 <glv11.glv11.glv11@gmail.com>
2022-06-30 16:44:28 +0300

    add tests

--- src/test/PetTest.java
+++ src/test/PetTest.java
@@ -52,8 +52,8 @@ class PetTest {
 //        happiness = Math.min(100, (happiness += 10));
 
         int hungryBefore = -1;
-        int happinessBefore = -1;
         int hungryAfter = -1;
+        int happinessBefore = -1;
         int happinessAfter = -1;
         int healthBefore = -1;
         int healthAfter = -1;
@@ -96,10 +96,94 @@ class PetTest {
 
     @org.junit.jupiter.api.Test
     void play() {
+
+        int hungryBefore = -1;
+        int hungryAfter = -1;
+        int happinessBefore = -1;
+        int happinessAfter = -1;
+        int healthBefore = -1;
+        int healthAfter = -1;
+
+        try {
+            Field hungry = pet.getClass().getDeclaredField("hungry");
+            hungry.setAccessible(true);
+            hungryBefore = (int)hungry.get(pet);
+            Field happiness = pet.getClass().getDeclaredField("happiness");
+            happiness.setAccessible(true);
+            happinessBefore = (int)happiness.get(pet);
+
+            Field health = pet.getClass().getDeclaredField("health");
+            health.setAccessible(true);
+            healthBefore = (int)health.get(pet);
+
+            pet.play();
+
+            hungryAfter = (int)hungry.get(pet);
+            happinessAfter = (int)happiness.get(pet);
+            healthAfter = (int)health.get(pet);
+
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            System.out.println("Exception! "+e.getMessage());
+            e.printStackTrace();
+        }
+
+
+//        hungry += 15;
+//        health = Math.min(100, (health+=5));
+//        happiness = Math.min(100, (happiness += 10));
+        int hungryExpected = (hungryBefore += 15);
+        int healthExpected = Math.min(100, (healthBefore+=5));
+        int happinessExpected = Math.min(100, (happinessBefore += 10));
+
+        Assertions.assertEquals(hungryExpected, hungryAfter);
+        Assertions.assertEquals(healthExpected,healthAfter);
+        Assertions.assertEquals(happinessExpected, happinessAfter);
+
     }
 
     @org.junit.jupiter.api.Test
     void sleep() {
+        int hungryBefore = -1;
+        int hungryAfter = -1;
+        int happinessBefore = -1;
+        int happinessAfter = -1;
+        int healthBefore = -1;
+        int healthAfter = -1;
+
+        try {
+            Field hungry = pet.getClass().getDeclaredField("hungry");
+            hungry.setAccessible(true);
+            hungryBefore = (int)hungry.get(pet);
+            Field happiness = pet.getClass().getDeclaredField("happiness");
+            happiness.setAccessible(true);
+            happinessBefore = (int)happiness.get(pet);
+
+            Field health = pet.getClass().getDeclaredField("health");
+            health.setAccessible(true);
+            healthBefore = (int)health.get(pet);
+
+            pet.sleep();
+
+            hungryAfter = (int)hungry.get(pet);
+            happinessAfter = (int)happiness.get(pet);
+            healthAfter = (int)health.get(pet);
+
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            System.out.println("Exception! "+e.getMessage());
+            e.printStackTrace();
+        }
+
+//        hungry += 10;
+//        health = Math.min(100, (health+=5));
+//        happiness = Math.min(100, (happiness += 5));
+        int hungryExpected = (hungryBefore += 10);
+        int healthExpected = Math.min(100, (healthBefore+=5));
+        int happinessExpected = Math.min(100, (happinessBefore += 5));
+
+        Assertions.assertEquals(hungryExpected, hungryAfter);
+        Assertions.assertEquals(healthExpected,healthAfter);
+        Assertions.assertEquals(happinessExpected, happinessAfter);
+
     }
 
     @org.junit.jupiter.api.Test
